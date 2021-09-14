758ee596abd6bc6867204db1bb6f3195cfd41f94
Roman Avdeienko <avroman91@gmail.com>
2021-09-14 19:17:09 +0300

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.io.File;
+
+
+public class Main {
+    public static void main(String[] args) {
+        read("./");
+        }
+
+    public static void read (String path){
+        File file = new File(path);
+        for (File f : file.listFiles()) {
+            if (f.isDirectory()) {
+                read(f.getPath());
+            }
+            if(!f.isDirectory() && f.getName().contains(".java")) System.out.println(f.getName());
+        }
+    }
+}
